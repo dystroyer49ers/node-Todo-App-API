5a81f7b7f3a69f78dbd44d7381bac11f0b785d8f
dystroyer49ers <34371700+dystroyer49ers@users.noreply.github.com>
2018-09-23 15:56:59 +0100

    Delete fuctions

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -6,10 +6,36 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 	}
 	console.log('Connected to MongoDB server');
 	const db = client.db('TodoApp');
+	
+	// // deleteMany
+	// db.collection('Todos').deleteMany({text: 'Eat Lunch'}).then((result) => {
+	//	console.log(result);
+	// });
 
-	db.collection('Todos').deleteMany({text: 'Eat Lunch'}).then((result) => {
-		console.log(result);
-	});
+	// // deleteOne
+	// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+	// 
 
+	// // findOneAndDelete
+	// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+	// 	console.log(result);
+	// });
+
+
+	// db.collection('Users').deleteMany({name: 'Zac'});
+
+	db.collection('Users').findOneAndDelete({
+		_id: new ObjectID("5ba5045c5d43d305bc459f26")
+	}).then((result) => {
+		console.log(JSON.stringify(result, undefined, 2));
+	});	
+	
+	
+	//User ID
+	// 5ba5045c5d43d305bc459f26
+	
+	// client.close();
 
 });
